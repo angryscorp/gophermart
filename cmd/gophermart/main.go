b9a02ef25486113decc20f39cc3d4029f75c1485
Anton Karpov <antony.karpov@icloud.com>
2025-06-22 15:16:01 +0300

    Refactor Bootstrapping

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -4,21 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"github.com/angryscorp/gophermart/internal/config"
-	"github.com/angryscorp/gophermart/internal/domain/model"
-	handlerAuth "github.com/angryscorp/gophermart/internal/http/handler/auth"
-	handlerBalance "github.com/angryscorp/gophermart/internal/http/handler/balance"
-	handlerOrders "github.com/angryscorp/gophermart/internal/http/handler/orders"
-	"github.com/angryscorp/gophermart/internal/http/router"
-	repositoryBalance "github.com/angryscorp/gophermart/internal/repository/balance"
-	"github.com/angryscorp/gophermart/internal/repository/migration"
-	repositoryOrders "github.com/angryscorp/gophermart/internal/repository/orders"
-	repositoryUsers "github.com/angryscorp/gophermart/internal/repository/users"
-	"github.com/angryscorp/gophermart/internal/usecase/accrual"
-	"github.com/angryscorp/gophermart/internal/usecase/auth"
-	"github.com/angryscorp/gophermart/internal/usecase/balance"
-	"github.com/angryscorp/gophermart/internal/usecase/orders"
-	"github.com/rs/zerolog"
-	"net/http"
 	"os"
 )
 
@@ -30,54 +15,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	zeroLogger := zerolog.New(os.Stdout).
-		Level(cfg.LogLevel()).
-		With().
-		Timestamp().
-		Logger()
-
-	if err := migration.Migrate(cfg.DatabaseDSN); err != nil {
-		_, _ = fmt.Fprint(os.Stderr, err.Error())
-		os.Exit(1)
-	}
-
-	usersRepository, err := repositoryUsers.New(cfg.DatabaseDSN)
+	server, err := bootstrap(&cfg)
 	if err != nil {
 		_, _ = fmt.Fprint(os.Stderr, err.Error())
 		os.Exit(1)
 	}
 
-	ordersRepository, err := repositoryOrders.New(cfg.DatabaseDSN)
-	if err != nil {
-		_, _ = fmt.Fprint(os.Stderr, err.Error())
-		os.Exit(1)
-	}
-
-	balanceRepository, err := repositoryBalance.New(cfg.DatabaseDSN)
-	if err != nil {
-		_, _ = fmt.Fprint(os.Stderr, err.Error())
-		os.Exit(1)
-	}
-
-	requestChan := make(chan string)
-	responseChan := make(chan *model.Accrual)
-
-	accrualAdapter := accrual.NewAdapter(&http.Client{}, zeroLogger, cfg.AccrualAddress)
-	accrualWorker := accrual.NewWorker(accrualAdapter, cfg.RateLimiter, requestChan, responseChan)
-	accrualWorker.Run()
-
-	authUsecase := auth.New(usersRepository, cfg.JWTSecret)
-	ordersUsecase := orders.New(ordersRepository, requestChan, responseChan, zeroLogger)
-	balanceUsecase := balance.New(balanceRepository)
-
-	r := router.New(zeroLogger, authUsecase)
-	r.RegisterAuth(handlerAuth.New(authUsecase, zeroLogger))
-	r.RegisterOrders(handlerOrders.New(ordersUsecase, zeroLogger))
-	r.RegisterBalance(handlerBalance.New(balanceUsecase, zeroLogger))
-
-	zeroLogger.Info().Str("address", cfg.ServerAddress).Msg("Application starting...")
-
-	err = r.Run(cfg.ServerAddress)
+	err = server.Run(cfg.ServerAddress)
 	if err != nil {
 		_, _ = fmt.Fprint(os.Stderr, err.Error())
 		os.Exit(1)
